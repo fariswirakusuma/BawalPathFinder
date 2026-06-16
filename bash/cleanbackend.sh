507405df85c1c3781bd6ad4ace895fb5c9d0d410
fariswirakusuma <fariswkt@gmail.com>
2026-06-16 10:21:51 +0700

    feat: add cleanbackend cmd

---
+++ bash/cleanbackend.sh
@@ -0,0 +1,19 @@
+# kill zombie process
+pkill -f ros2
+pkill -f planner_server
+pkill -f controller_server
+pkill -f behavior_server
+pkill -f bt_navigator
+pkill -f smoother_server
+pkill -f waypoint_follower
+pkill -f velocity_smoother
+pkill -f tf2_ros
+pkill -f component_container
+pkill -f rosbridge
+
+sleep 2
+
+WORKSPACE_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+cd "$WORKSPACE_ROOT/ROS_workspace" || exit 1
+
+rm -rf build/ install/ log/
